681f01455525347fb0e6883e9665ff82c34ca61a
stefan <stefank17@ru.is>
2021-06-01 16:43:23 +0000

    update

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 try {
   $name = $_GET['name'];
   $url = "https://apis.is/company?name={$name}";
-  header('Content-Type: application/json');
+  header('Access-Control-Allow-Headers: X-Requested-With, Accept, Origin, Referer, User-Agent, Content-Type, Authorization, X-Mindflash-SessionID');
+  header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE');
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json; charset=utf-8');
 
   $response = file_get_contents($url);
 
